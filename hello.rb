97b4238bada5794c0311aea6acdf8a00c72666e7
KANEKO529 <kanekohiromsa529@gmail.com>
2024-04-06 14:19:44 +0900

    second commit

--- hello.rb
+++ hello.rb
@@ -10,10 +10,11 @@ else
     puts "confortable!"
 end
 
-array = [6, 7, 3, 2, 5, 4, 1]
+#基本交換法
+array = [6, 7, 3, 2, 5, 4, 1]#配列
 array_size = array.size
 
-array_size.times do |i|
+array_size.times do |i|#繰り返し処理　for文
   (array_size - (i + 1)).times do |j|
     if array[j] > array[j + 1]
       escape = array[j]
@@ -24,3 +25,13 @@ array_size.times do |i|
 end
 
 p array
+
+temp = 30 #変数
+
+if temp >= 30 then
+    puts "hot!"
+elsif temp <= 20 then
+    puts "cold!"
+else 
+    puts "confortable!"
+end
